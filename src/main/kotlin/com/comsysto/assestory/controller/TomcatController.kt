163835e02b01bc0970fbbbc05940a06f5f9a043d
cs-andreikalach <andrei.kalach@comsysto.com>
2017-11-24 10:41:13 +0100

    Tomcat Page : possibility to change the state of an Tomcat instance

--- src/main/kotlin/com/comsysto/assestory/controller/TomcatController.kt
+++ src/main/kotlin/com/comsysto/assestory/controller/TomcatController.kt
@@ -1,17 +1,18 @@
 package com.comsysto.assestory.controller
 
-import com.comsysto.assestory.data.EnvironmentRepository
 import com.comsysto.assestory.data.TomcatRepository
-import com.comsysto.assestory.data.domain.Environment
+import com.comsysto.assestory.data.domain.State
+import com.comsysto.assestory.data.domain.Tomcat
 import org.springframework.http.MediaType
-import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
 
 @Controller
 class TomcatController(private val tomcatRepository: TomcatRepository) {
@@ -20,8 +21,18 @@ class TomcatController(private val tomcatRepository: TomcatRepository) {
     fun findAll() = tomcatRepository.findAll()
 
     @GetMapping(value = "/Tomcats", produces = arrayOf(MediaType.TEXT_HTML_VALUE))
-    fun findAllView(model : Model) : String {
+    fun findAllView(model: Model): String {
         model.addAttribute("tomcats", tomcatRepository.findAll())
         return "tomcats"
+    }
+
+    @RequestMapping(value = "/maintenance", method = arrayOf(RequestMethod.POST))
+    fun postSearch(request: HttpServletRequest): String {
+        val tomcatId = request.getParameter("tomcat_id")
+        val tomcatInstance : Tomcat = tomcatRepository.findById(tomcatId).get()
+        tomcatInstance.state = if (tomcatInstance.state == State.Offline) State.Online else State.Offline
+        tomcatRepository.save(tomcatInstance)
+
+        return "redirect:Tomcats"
     }
 }
